69de6fae1068b9672d98215bb4d8044053923940
Julianmotta07 <mottajulian47@gmail.com>
2024-09-11 20:07:41 -0500

    feat: creación del metodo para imprimir vehiculos

--- CarManagement/src/Main.java
+++ CarManagement/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
 
+    public void imprimirVehiculos() {
+        for (Vehiculo vehiculo : vehiculos) {
+            System.out.println(vehiculo);
+        }
+    }
+
     public static void main(String[] args) {
         
     }
